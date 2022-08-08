6c088233f6846f4721afef5acfa341a7a19945bf
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2022-08-08 23:14:20 +0200

    Clean global + contact page

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,14 +2,13 @@ const $ = document
 
 $.addEventListener("DOMContentLoaded", () => {
 	const yearSet = () => {
-		const year = document.getElementById("year")
+		const year = document.getElementById("yearDisplay")
 		const d = new Date().getFullYear()
 		year.innerHTML = d
 		// year.addEventListener("click", () => {
-		// 	console.log(d)
+		// 	console.log("test")
 		// })
 	}
-
 	yearSet()
 
 	const copyClipboard = () => {
@@ -120,39 +119,39 @@ $.addEventListener("DOMContentLoaded", () => {
 		submitButton.classList.add("disabled-btn")
 	}
 
-	form.addEventListener("submit", async (event) => {
-		event.preventDefault()
-		isDisabled()
+	// form.addEventListener("submit", async (event) => {
+	// 	event.preventDefault()
+	// 	isDisabled()
 
-		const data = {
-			firstname: $.querySelector("#firstname").value,
-			lastname: $.querySelector("#lastname").value,
-			email: $.querySelector("#email").value,
-			subject: $.querySelector("#subject").value,
-			message: $.querySelector("#message").value,
-		}
+	// 	const data = {
+	// 		firstname: $.querySelector("#firstname").value,
+	// 		lastname: $.querySelector("#lastname").value,
+	// 		email: $.querySelector("#email").value,
+	// 		subject: $.querySelector("#subject").value,
+	// 		message: $.querySelector("#message").value,
+	// 	}
 
-		try {
-			// const response = await axios.post("http://localhost:3000/form", data);
-			const response = await axios.post(
-				"https://formulaire-backend-mf.herokuapp.com/form",
-				data
-			)
+	// 	try {
+	// 		// const response = await axios.post("http://localhost:3000/form", data);
+	// 		const response = await axios.post(
+	// 			"https://formulaire-backend-mf.herokuapp.com/form",
+	// 			data
+	// 		)
 
-			if (response.status === 200) {
-				alert("Votre formulaire a bien été envoyé")
-				cleanForm()
-				isEnabled()
-			}
-		} catch (e) {
-			if (e.response.data.error === "Missing parameters") {
-				alert("Veuillez remplir tous les champs du formulaire")
-			} else {
-				alert("Une erreur est survenue")
-				cleanForm()
-			}
+	// 		if (response.status === 200) {
+	// 			alert("Votre formulaire a bien été envoyé")
+	// 			cleanForm()
+	// 			isEnabled()
+	// 		}
+	// 	} catch (e) {
+	// 		if (e.response.data.error === "Missing parameters") {
+	// 			alert("Veuillez remplir tous les champs du formulaire")
+	// 		} else {
+	// 			alert("Une erreur est survenue")
+	// 			cleanForm()
+	// 		}
 
-			isEnabled()
-		}
-	})
+	// 		isEnabled()
+	// 	}
+	// })
 })
