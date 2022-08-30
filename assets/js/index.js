491c78a8d8ab0a66e8977ab99feb83f303f89620
manuelfontenelle <manuel.fontenelle@gmail.com>
2022-08-30 14:34:20 +0200

    fix correction texte + year copyright

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,185 +1,187 @@
-const $ = document
+const $ = document;
 
 $.addEventListener("DOMContentLoaded", () => {
-	const randomFonction = () => {
-		const months = [
-			"deluxe-rose-&-bleue",
-			"lausanne-jardin",
-			"le-flipot",
-			"le-jardinier",
-			"fontaine-mairie-de-mauregard",
-			"skolem",
-			"bouche-oreille",
-			"disque-double",
-			"sans-titre",
-			"mairie-beganne",
-			"lumps-bumps-and-windy-too",
-			"palme-jetfin-&-porte-palme",
-			"sculpture-portable",
-			"pet-&-troll",
-			"sculpture-faïence",
-			"sculpture-porcelaine",
-			"jardinière",
-			"pense-bête",
-			"cod",
-			"étagère",
-			"jet-fin",
-			"occupation-des-sols",
-			"espaces-combinés-turbibulle-&-turbo-mollo",
-			"gaufrier",
-			"volume-en-briques",
-			"os",
-			"polyplac-&-velamen",
-			"mémo-calendar",
-			"bat",
-			"wing",
-			"disque",
-			"phenemenon",
-			"knights",
-			"toupille",
-			"organe",
-			"wings",
-		]
-		const random = Math.floor(Math.random() * months.length)
-		const randomProject = document.getElementById("random-project")
-		const randomProjectLink = document.getElementById("random-project-link")
-		if (randomProject != null) {
-			randomProject.textContent = (random, months[random].replaceAll("-", " "))
-			randomProjectLink.setAttribute(
-				"href",
-				"./" + (random, months[random]) + ".html"
-			)
-		}
-	}
-	randomFonction()
+  const randomFonction = () => {
+    const months = [
+      "deluxe-rose-&-bleue",
+      "lausanne-jardin",
+      "le-flipot",
+      "le-jardinier",
+      "fontaine-mairie-de-mauregard",
+      "skolem",
+      "bouche-oreille",
+      "disque-double",
+      "sans-titre",
+      "mairie-beganne",
+      "lumps-bumps-and-windy-too",
+      "palme-jetfin-&-porte-palme",
+      "sculpture-portable",
+      "pet-&-troll",
+      "sculpture-faïence",
+      "sculpture-porcelaine",
+      "jardinière",
+      "pense-bête",
+      "cod",
+      "étagère",
+      "jet-fin",
+      "occupation-des-sols",
+      "espaces-combinés-turbibulle-&-turbo-mollo",
+      "gaufrier",
+      "volume-en-briques",
+      "os",
+      "polyplac-&-velamen",
+      "mémo-calendar",
+      "bat",
+      "wing",
+      "disque",
+      "phenemenon",
+      "knights",
+      "toupille",
+      "organe",
+      "wings",
+    ];
+    const random = Math.floor(Math.random() * months.length);
+    const randomProject = document.getElementById("random-project");
+    const randomProjectLink = document.getElementById("random-project-link");
+    if (randomProject != null) {
+      randomProject.textContent = (random, months[random].replaceAll("-", " "));
+      randomProjectLink.setAttribute(
+        "href",
+        "./" + (random, months[random]) + ".html"
+      );
+    }
+  };
+  randomFonction();
 
-	const yearSet = () => {
-		const year = document.getElementById("yearDisplay")
-		const d = new Date().getFullYear()
-		year.innerHTML = d
-	}
-	yearSet()
+  const yearSet = () => {
+    const year = document.getElementById("yearDisplay");
+    const year2 = document.getElementById("yearDisplay2");
+    const d = new Date().getFullYear();
+    year.innerHTML = d;
+    year2.innerHTML = d;
+  };
+  yearSet();
 
-	const copyClipboard = () => {
-		const mailToCopy = document.getElementById("mailToCopy")
-		mailToCopy.addEventListener("click", () => {
-			const range = document.createRange()
-			range.selectNode(mailToCopy)
-			window.getSelection().removeAllRanges() // clear current selection
-			window.getSelection().addRange(range) // to select text
-			$.execCommand("copy")
-			window.getSelection().removeAllRanges() // to deselect
-			// console.log("test")
-		})
-	}
-	copyClipboard()
+  const copyClipboard = () => {
+    const mailToCopy = document.getElementById("mailToCopy");
+    mailToCopy.addEventListener("click", () => {
+      const range = document.createRange();
+      range.selectNode(mailToCopy);
+      window.getSelection().removeAllRanges(); // clear current selection
+      window.getSelection().addRange(range); // to select text
+      $.execCommand("copy");
+      window.getSelection().removeAllRanges(); // to deselect
+      // console.log("test")
+    });
+  };
+  copyClipboard();
 
-	const copytootTip = () => {
-		const mailToCopy = document.getElementById("mailToCopy")
-		const tooltipText = document.getElementById("tooltiptext")
-		mailToCopy.addEventListener("click", () => {
-			tooltipText.innerHTML = "Copié !"
-		})
-	}
-	copytootTip()
+  const copytootTip = () => {
+    const mailToCopy = document.getElementById("mailToCopy");
+    const tooltipText = document.getElementById("tooltiptext");
+    mailToCopy.addEventListener("click", () => {
+      tooltipText.innerHTML = "Copié !";
+    });
+  };
+  copytootTip();
 
-	const BurgerMenuFunction = () => {
-		const burgerContainer = document.getElementById("burger-container")
-		const burgerMenu = document.getElementById("burger")
-		const menuLeft = document.getElementById("menu-left")
+  const BurgerMenuFunction = () => {
+    const burgerContainer = document.getElementById("burger-container");
+    const burgerMenu = document.getElementById("burger");
+    const menuLeft = document.getElementById("menu-left");
 
-		burgerContainer.addEventListener("click", () => {
-			// console.log("test")
-			menuOpen = document.getElementById("menu-open")
-			menuClose = document.getElementById("menu-close")
+    burgerContainer.addEventListener("click", () => {
+      // console.log("test")
+      menuOpen = document.getElementById("menu-open");
+      menuClose = document.getElementById("menu-close");
 
-			if (menuOpen.classList.contains("active")) {
-				// console.log("test2")
-				menuOpen.classList.remove("active")
-				menuClose.classList.add("active")
-				burgerMenu.classList.add("open")
-				menuLeft.classList.add("slide-in")
-			} else {
-				menuOpen.classList.add("active")
-				menuClose.classList.remove("active")
-				burgerMenu.classList.remove("open")
-				menuLeft.classList.remove("slide-in")
-			}
-		})
+      if (menuOpen.classList.contains("active")) {
+        // console.log("test2")
+        menuOpen.classList.remove("active");
+        menuClose.classList.add("active");
+        burgerMenu.classList.add("open");
+        menuLeft.classList.add("slide-in");
+      } else {
+        menuOpen.classList.add("active");
+        menuClose.classList.remove("active");
+        burgerMenu.classList.remove("open");
+        menuLeft.classList.remove("slide-in");
+      }
+    });
 
-		burgerMenu.addEventListener("click", () => {
-			burgerMenu.classList.toggle("open")
-		})
-	}
-	BurgerMenuFunction()
+    burgerMenu.addEventListener("click", () => {
+      burgerMenu.classList.toggle("open");
+    });
+  };
+  BurgerMenuFunction();
 
-	const scrollTopFunction = () => {
-		const mybutton = document.getElementById("scrollTop")
-		mybutton.addEventListener("click", () => {
-			console.log("test")
-			window.scrollTo({ top: 0, behavior: "smooth" })
-		})
-	}
-	scrollTopFunction()
+  const scrollTopFunction = () => {
+    const mybutton = document.getElementById("scrollTop");
+    mybutton.addEventListener("click", () => {
+      console.log("test");
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  };
+  scrollTopFunction();
 
-	// topFunction()
+  // topFunction()
 
-	const formFunction = () => {
-		const form = $.querySelector("#contact-form")
-		const submitButton = $.querySelector("#submit-btn")
-		// Fonction pour vider les champs du formulaire :
-		const cleanForm = () => {
-			form.reset()
-		}
+  const formFunction = () => {
+    const form = $.querySelector("#contact-form");
+    const submitButton = $.querySelector("#submit-btn");
+    // Fonction pour vider les champs du formulaire :
+    const cleanForm = () => {
+      form.reset();
+    };
 
-		// Fonction pour réactiver le bouton d'envoi du formulaire :
-		const isEnabled = () => {
-			submitButton.removeAttribute("disabled")
-			submitButton.classList.remove("disabled-btn")
-		}
+    // Fonction pour réactiver le bouton d'envoi du formulaire :
+    const isEnabled = () => {
+      submitButton.removeAttribute("disabled");
+      submitButton.classList.remove("disabled-btn");
+    };
 
-		// Fonction pour désactiver le bouton d'envoi du formulaire :
-		const isDisabled = () => {
-			submitButton.setAttribute("disabled", "disabled")
-			submitButton.classList.add("disabled-btn")
-		}
+    // Fonction pour désactiver le bouton d'envoi du formulaire :
+    const isDisabled = () => {
+      submitButton.setAttribute("disabled", "disabled");
+      submitButton.classList.add("disabled-btn");
+    };
 
-		form.addEventListener("submit", async (event) => {
-			event.preventDefault()
-			isDisabled()
+    form.addEventListener("submit", async (event) => {
+      event.preventDefault();
+      isDisabled();
 
-			const data = {
-				email: $.querySelector("#email").value,
-				message: $.querySelector("#message").value,
-			}
+      const data = {
+        email: $.querySelector("#email").value,
+        message: $.querySelector("#message").value,
+      };
 
-			try {
-				// const response = await axios.post(
-				// 	"http://localhost:3000/form",
-				// 	data,
-				// )
-				const response = await axios.post(
-					"https://nodemailer-backend-mf.herokuapp.com/form",
-					data
-				)
+      try {
+        // const response = await axios.post(
+        // 	"http://localhost:3000/form",
+        // 	data,
+        // )
+        const response = await axios.post(
+          "https://nodemailer-backend-mf.herokuapp.com/form",
+          data
+        );
 
-				if (response.status === 200) {
-					alert("Votre formulaire a bien été envoyé")
-					cleanForm()
-					isEnabled()
-				}
-			} catch (e) {
-				if (e.response.data.error === "Missing parameters") {
-					alert("Veuillez remplir tous les champs du formulaire")
-				} else {
-					alert("Une erreur est survenue")
-					cleanForm()
-				}
+        if (response.status === 200) {
+          alert("Votre formulaire a bien été envoyé");
+          cleanForm();
+          isEnabled();
+        }
+      } catch (e) {
+        if (e.response.data.error === "Missing parameters") {
+          alert("Veuillez remplir tous les champs du formulaire");
+        } else {
+          alert("Une erreur est survenue");
+          cleanForm();
+        }
 
-				isEnabled()
-			}
-		})
-	}
+        isEnabled();
+      }
+    });
+  };
 
-	formFunction()
-})
+  formFunction();
+});
